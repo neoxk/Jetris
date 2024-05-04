25415cf6fceb56421257c2b4590277a868d726a6
NXK <neo.kirbis@gmail.com>
2024-05-04 21:42:12 +0200

    removed moving logic from Square

--- src/jetris/Square.java
+++ src/jetris/Square.java
@@ -7,12 +7,10 @@ import java.awt.geom.Path2D;
 public class Square{
     private int x;
     private int y;
-    private Color color;
 
     public Square(int x, int y, Color color) {
        this.x = x * Const.SQUARE_SIZE;
        this.y = y * Const.SQUARE_SIZE;
-       this.color = color;
     }
 
     public Shape getShape() {
@@ -25,18 +23,19 @@ public class Square{
         return path;
     }
 
-
-
-    public void newFrame() {
-        this.y += Const.SQUARE_SIZE;
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
     }
 
-    public void moveLeft() {
-        this.x -= Const.SQUARE_SIZE;
-    }
-    public void moveRight() {
-        this.x += Const.SQUARE_SIZE;
-    }
 
     public static Shape merge(Square[] squares) {
         GeneralPath mergedPath = new GeneralPath();
